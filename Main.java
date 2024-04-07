922625a03dca4897ff348b83dc9c20ea239d2b0c
shiro_pc <przemek.fied2@gmail.com>
2024-04-07 11:35:33 +0200

    zad5

--- Main.java
+++ Main.java
@@ -1,23 +1,15 @@
 import geometry.Point;
 import geometry.Circle;
 import geometry.ColoredCircle;
+import geometry.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Circle[] circles = new Circle[3];
-        circles[0] = new Circle(new Point(0, 0), 2.0);
-        circles[1] = new Circle(new Point(1, 1), 3.0);
-        circles[2] = new ColoredCircle(new Point(2, 2), 4.0, "blue");
+        Rectangle rectangle = new Rectangle(3.0, 5.0);
         
-        for (Circle circle : circles) {
-            double area = circle.getArea();
-            System.out.println("Area of the circle: " + area);
-            
-            if (circle instanceof ColoredCircle) {
-                ColoredCircle coloredCircle = (ColoredCircle) circle;
-                String color = coloredCircle.getColor();
-                System.out.println("Color of the circle: " + color);
-            }
-        }
+        System.out.println("Length of the rectangle: " + rectangle.getLength());
+        System.out.println("Width of the rectangle: " + rectangle.getWidth());
+        System.out.println("Area of the rectangle: " + rectangle.calculateArea());
+        System.out.println("Perimeter of the rectangle: " + rectangle.calculatePerimeter());
     }
 }
---
+++ geometry/Rectangle.java
@@ -0,0 +1,34 @@
+package geometry;
+
+public class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
