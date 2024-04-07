5f83d7e638215ab2c8d6c6c25009dca514858991
shiro_pc <przemek.fied2@gmail.com>
2024-04-07 11:13:36 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
-
+import geometry.Point;
+import geometry.Circle;
 
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(3.5, 2.0);
-        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+        System.out.println("Point coordinates: (" + point.getX() + ", " + point.getY() + ")");
+
+        Circle circle = new Circle(point, 5.0);
+        System.out.println("Center of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Radius of the circle: " + circle.getRadius());
+        System.out.println("Perimeter of the circle: " + circle.calculatePerimeter());
+        System.out.println("Area of the circle: " + circle.getArea());
     }
 }
--- geometry/Point.java
+++ geometry/Point.java
@@ -1,3 +1,5 @@
+package geometry;
+
 public class Point {
     private double x;
     private double y;
