f5b6e9abd28bbdcbf1e7515566d906148d62dcfe
shiro_pc <przemek.fied2@gmail.com>
2024-04-07 11:03:23 +0200

    init

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 2.0);
+        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
